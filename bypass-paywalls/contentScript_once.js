ca0d90c24d6f881fb766f5d10a11f4478e1d5a93
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2022-12-08 19:20:19 -0500

    Final Comit

---
+++ bypass-paywalls/contentScript_once.js
@@ -0,0 +1,61 @@
+// "use strict";
+
+if (matchDomain('github.com')) {
+  window.setTimeout(function () {
+    let bio = document.querySelector('div.profile-user-bio');
+    if (bio) {
+      let split = bio.innerText.split(/(https:[\w\-/.]+)|\|/g).filter(x => x && x.trim());
+      bio.innerText = '';
+      for (let part of split) {
+        let elem;
+        if (part.startsWith('https')) {
+          elem = document.createElement('a');
+          elem.innerText = part;
+          elem.href = part;
+          elem.appendChild(document.createElement('br'));
+        } else {
+          elem = document.createElement('b');
+          elem.appendChild(document.createTextNode(part));
+          if (!part.includes(':')) {
+            elem.appendChild(document.createElement('br'));
+          }
+        }
+        bio.appendChild(elem);
+      }
+    }
+  }, 1000);
+}
+
+else if (matchDomain('yassertabora.netlify.app')) {
+  function nzherald_main() {
+    if (window.Fusion) {
+      window.Fusion.globalContent.isPremium = false;
+    }
+  }
+  window.setTimeout(function () {
+    insert_script(nzherald_main);
+  }, 100);
+}
+
+function matchDomain(domains, hostname) {
+  var matched_domain = false;
+  if (!hostname) {
+    hostname = window.location.hostname;
+  }
+  if (typeof domains === 'string') {
+    domains = [domains];
+  }
+  domains.some(domain => (hostname === domain || hostname.endsWith('.' + domain)) && (matched_domain = domain));
+  return matched_domain;
+}
+
+function insert_script(func, insertAfterDom) {
+  let bpc_script = document.querySelector('script#bpc_script');
+  if (!bpc_script) {
+    let script = document.createElement('script');
+    script.setAttribute('id', 'bpc_script');
+    script.appendChild(document.createTextNode('(' + func + ')();'));
+    let insertAfter = insertAfterDom ? insertAfterDom : (document.body || document.head || document.documentElement);
+    insertAfter.appendChild(script);
+  }
+}
