17ad17ca5d46c2a8a63b2c2c4673ea471f478665
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-01-26 19:48:35 -0500

    Final Commit

---
+++ bypass-paywalls/options/clearCookies.js
@@ -0,0 +1,40 @@
+var ext_api = (typeof browser === 'object') ? browser : chrome;
+
+window.localStorage.clear();
+sessionStorage.clear();
+
+var cookie_domain = getCookieDomain(document.domain);
+
+// Send Domain To background.js (To Clear Cookies)
+ext_api.runtime.sendMessage({
+  request: 'clear_cookies_domain',
+  data: {
+    domain: cookie_domain
+  }
+});
+
+function getCookieDomain(hostname) {
+  let domain = hostname;
+  let n = 0;
+  let parts = hostname.split('.');
+  let str = '_gd' + (new Date()).getTime();
+  while (n < (parts.length - 1) && document.cookie.indexOf(str + '=' + str) == -1) {
+    domain = parts.slice(-1 - (++n)).join('.');
+    document.cookie = str + "=" + str + ";domain=" + domain + ";";
+  }
+  document.cookie = str + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=" + domain + ";";
+  return domain;
+}
+
+var msg = "Cookies (and local storage) removed from " + cookie_domain;
+showMessage(msg, 2000);
+
+function showMessage(msg, duration) {
+  var el = document.createElement("div");
+  el.setAttribute("style", "position: fixed; top: 40%; left: 40%; z-index: 99; padding: 4px; font-family: Arial, sans-serif; font-size: 18px; color: white; background-color: blue;");
+  el.innerText = msg;
+  setTimeout(function() {
+    el.parentNode.removeChild(el);
+  }, duration);
+  (document.body || document.head || document.documentElement).appendChild(el);
+}
